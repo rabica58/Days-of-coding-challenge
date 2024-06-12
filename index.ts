12751601c962548d4e00b58a7de3300579e95296
rabica58 <rabicajennie58@gmail.com>
2024-06-12 00:55:23 -0700

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,43 @@
+//                  *Day 12  of coding challenge*   //
+
+//  Question 1: Calculate arange percentage 
+
+// find the average Grade: Given a list of grades, calculate the average grade.
+
+let grades = [74, 55, 90, 45, 55];
+
+let averageGrade = grades.reduce ((total,grade) => total + grade, 0/ grades.length);
+console.log(averageGrade);
+
+ //               *Day 13 of coding challenge*   //
+ 
+ // Question 2: Average Score Calculator: Write a simple programe that can take lots of 
+ // scores and find their average.
+
+ function averageScore(... scores: number[]): number {
+    let total = scores.reduce((sum, score) => sum + score, 0);
+    return total / scores.length;
+ }
+// Example: finding the average of four scores.
+console.log(averageScore(50, 80, 100, 60));
+
+//                     *Day 14 of coding challenge*   //
+
+// Question 3: Making Flexible Object Keys: Learn how to set up a list where you can
+// change the name of each section based on what you need at the moment.
+
+function objectKey(key: string, value: string) {
+    let dynamicObject = {}; 
+    dynamicObject[key] = value;
+    return dynamicObject;
+}
+let userPreference = objectKey("theme", "dark");
+console.log(userPreference);
+
+
+
+
+
+
+
+
